e4b878639a4a9ad07e78857778000a996c6f989b
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-09-14 00:45:44 +0000

    5th save

--- src/app.js
+++ src/app.js
@@ -5,9 +5,60 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//window.onload = function() {
+//btn.addEventListener("click", function() {
+//generatorCard();
+//});
+//};
+console.log("Hello Rigo from the console!");
+const generatorCard = () => {
+  let title = document.querySelector(".title");
+  let emoji = ["♦", "♣", "♥", "♠"];
+  let number = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let randomEmoji = emoji[Math.floor(Math.random() * emoji.length)];
+
+  if (randomEmoji === "💃🏽") {
+    btn.style.background = "red";
+    btn.style.color = "white";
+    title.innerHTML = "Felicia";
+  } else if (randomEmoji === "🦊") {
+    btn.style.color = "white";
+    btn.style.background = "orange";
+    title.innerHTML = "Foxy Brown";
+  } else if (randomEmoji === "🐥") {
+    btn.style.color = "black";
+    btn.style.background = "yellow";
+    title.innerHTML = "Norbert";
+  } else if (randomEmoji === "🐸") {
+    btn.style.color = "white";
+    btn.style.background = "green";
+    title.innerHTML = "Robert";
+  } else if (randomEmoji === "🪼") {
+    btn.style.color = "white";
+    btn.style.background = "blue";
+    title.innerHTML = "Welches";
+  } else {
+    btn.style.background = "purple";
+    btn.style.color = "white";
+    title.innerHTML = "Ella";
+  }
+
+  document.querySelector(".emoji").innerHTML = randomEmoji;
 };
 //♦
 //♣
