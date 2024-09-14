b8aef6218458af45836a6c1dd6c93503a5fbc8e4
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-09-14 00:11:05 +0000

    save

--- src/app.js
+++ src/app.js
@@ -9,3 +9,13 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+//♦
+//♣
+//♥
+//♠
+//1.) make work as deck
+//2.) use suits instead of number
+//3.) animate switching between cards
+//1.) have button generate the new card
+//2.) the color of the number needs to match the suit (red and black)
+//3.) make it pretty
